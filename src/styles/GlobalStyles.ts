2be3e154a212b6b461fad3e12097389a5f892039
Caio Maqueise Alécio Pinheiro <caio.maqueise@gmail.com>
2021-12-06 22:59:49 -0400

    config vars theme color

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -1,6 +1,13 @@
 import { createGlobalStyle } from 'styled-components'
 
 export default createGlobalStyle`
+:root {
+  --primary: #59518C;
+  --secundary:#59273F;
+  --bg-light:#C2BFD9;
+  font-size: 16px;
+
+}
 *{
     padding: 0;
     margin: 0;
@@ -8,9 +15,11 @@ export default createGlobalStyle`
 }
 html,body{
     min-height:100vh;
+    width: 100vw;
 }
 body{
     font-family: 'Roboto', sans-serif;
+    background-color: var(--primary);
 }
 h1,h2,h3{
     font-family: 'Poppins',sans-serif;
